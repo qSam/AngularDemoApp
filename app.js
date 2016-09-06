1e661626e80eb693f0347aa626a14d3c3967a0ef
Qasim Laghari <go.jets.laghari@gmail.com>
2016-09-06 17:45:04 -0500

    Update Controller

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope', function ($scope) {
-    
+    $scope.name = 'James';
+    console.log('The scope is : ',$scope);
 }]);
-
-
-
