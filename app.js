68474cfd605b6cdcf6e40a63fe30a89c42f11684
Qasim Laghari <go.jets.laghari@gmail.com>
2016-09-07 14:52:54 -0500

    Add  Angular service

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', function ($scope) {
+angularApp.controller('mainController', function ($scope,$log) {
     $scope.name = 'James';
     console.log('The scope is : ',$scope);
-}]);
+    console.log($log);
+});
