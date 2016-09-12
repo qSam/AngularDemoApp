87c41bc0be0cce9497693fa81f85bb9e583f9333
Qasim Laghari <go.jets.laghari@gmail.com>
2016-09-12 14:23:20 -0500

    Directives and Two Way data binding

--- app.js
+++ app.js
@@ -2,10 +2,15 @@
 var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
-angularApp.controller('mainController', ['$scope', '$timeout', function ($scope, $timeout) {
+angularApp.controller('mainController', ['$scope', '$timeout', '$filter', function ($scope, $timeout, $filter) {
     $scope.name = 'Qasim';
 
     $timeout(function(){
-      $scope.name = 'Wait, you are not Q'  
+      $scope.name = 'Wait, you are not Q'
     }, 3000);
+
+    $scope.handle = '';
+    $scope.lowercasehandle = function() {
+      return $filter('lowercase')($scope.handle);
+    };
 }]);
